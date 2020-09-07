bc03c3aaa9b8523d6a857341fbc8f228a32b62c0
aospiridonov <aospiridonov@gmail.com>
2020-09-07 22:31:03 +0300

    fix height TramsactionList container

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,7 +11,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300.0,
+      height: 450.0,
       child: transations.isEmpty
           ? Column(
               children: <Widget>[
